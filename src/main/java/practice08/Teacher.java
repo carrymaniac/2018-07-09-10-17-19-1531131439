955293fabd0913b9846d58c14ecff2a640526144
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 16:42:34 +0800

    [henry][feature]pass the test  should_teacher_introduce_a_student_it_teaches in practice08

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,6 +1,5 @@
 package practice08;
 
-
 public class Teacher extends Person{
     Klass klass;
     public Teacher(Integer id, String name, Integer age, Klass klass) {
@@ -27,5 +26,13 @@ public class Teacher extends Person{
         } else {
             return super.introduce() + " I am a Teacher. I teach " + this.klass.getDisplayName() + ".";
         }
+    }
+
+    public String introduceWith(Student student) {
+        return student.klass.equals(this.klass) ?
+                "My name is Tom. I am 21 years old. I am a Teacher. I teach "+student.getName()+"."
+                :
+                "My name is Tom. I am 21 years old. I am a Teacher. I don't teach "+student.getName()+"."
+                ;
     }
 }
