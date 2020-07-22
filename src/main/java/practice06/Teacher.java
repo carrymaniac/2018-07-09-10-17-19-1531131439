45e06381dc4f4d28152a3ae851dfcf43c8b29e0c
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 16:00:38 +0800

    [henry][feature]pass the test  should_teacher_have_name_and_age_and_class in practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,22 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    Integer klass;
+
+    public Teacher(String name, Integer age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
 }
