1814a87968dd30f753df97373784580dbd2c11ac
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 16:31:04 +0800

    [henry][feature]pass the test  should_class_assign_a_leader in practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -2,7 +2,7 @@ package practice08;
 
 public class Klass {
     Integer number;
-
+    Student leader;
     public Klass() {
     }
 
@@ -20,5 +20,11 @@ public class Klass {
 
     public String getDisplayName() {
         return "Class "+number;
+    }
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
+    public Student getLeader(){
+        return this.leader;
     }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,23 @@
 package practice08;
 
-public class Student {
+
+public class Student extends Person {
+    Klass klass;
+
+    public Student(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Student(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
