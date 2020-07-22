dac55ed58403a48f45ad60d649a926f0098a279e
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 16:24:39 +0800

    [henry][feature]pass the test  should_person_with_same_id_be_same_one in practice08

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,5 +1,7 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
     Integer id;
     String name;
@@ -33,6 +35,21 @@ public class Person {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(age, person.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
     }
 
     public String introduce(){
