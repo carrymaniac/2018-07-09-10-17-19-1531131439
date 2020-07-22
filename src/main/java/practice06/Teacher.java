747291e632c7b2b62fca5d856ecc17a50c3ed587
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 16:01:42 +0800

    [henry][feature]pass the test  should_teacher_introduce_itself_with_which_class_it_teaches in practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -19,4 +19,9 @@ public class Teacher extends Person{
     public void setKlass(Integer klass) {
         this.klass = klass;
     }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Teacher. I teach Class "+this.klass+".";
+    }
 }
