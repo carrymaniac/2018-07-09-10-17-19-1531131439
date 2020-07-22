3ef28e5b1b3dc01c811dc03a471fed904e559048
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 15:59:02 +0800

    [henry][feature]pass the test  should_student_have_name_age_and_class_number in practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Student {
+import practice05.Person;
+
+public class Student extends Person {
+    Integer klass;
+    public Student(String name, Integer age) {
+        super(name, age);
+    }
+
+    public Student(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class "+this.klass+".";
+    }
 }
