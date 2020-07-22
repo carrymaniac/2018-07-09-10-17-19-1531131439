8e93b27de6538cfce5f87ffe774151ea6684e503
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 19:10:26 +0800

    [henry][feature]pass the test  should_teacher_have_name_and_age_and_classes in practice11

--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,41 @@
 package practice11;
 
-public class Teacher {
+import practice09.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person{
+    private List<Klass> classes = new ArrayList<>();
+
+    public Teacher(Integer id, String name, Integer age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+    @Override
+    public String introduce() {
+        if (classes.size()==0) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            String collect = classes.stream().map(klass -> String.valueOf(klass.number)).collect(Collectors.joining(","));
+            return super.introduce() + " I am a Teacher. I teach " + collect + ".";
+        }
+    }
+
+//    public String introduceWith(Student student) {
+//        return student.klass.equals(this.klass) ?
+//                "My name is Tom. I am 21 years old. I am a Teacher. I teach "+student.getName()+"."
+//                :
+//                "My name is Tom. I am 21 years old. I am a Teacher. I don't teach "+student.getName()+"."
+//                ;
+//    }
 }
--- src/test/java/practice11/Practice11Test.java
+++ src/test/java/practice11/Practice11Test.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.LinkedList;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -92,17 +93,17 @@ public class Practice11Test {
         klass2.assignLeader(tom);
         assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.");
     }
-//
-//    @Test
-//    public void should_teacher_have_name_and_age_and_classes() throws Exception {
-//        LinkedList<practice11.Klass> linkedList = new LinkedList<Klass>();
-//        linkedList.add(klass2);
-//        Teacher tom = new Teacher(1, "Tom", 21, linkedList);
-//        assertThat(tom.getName()).isEqualTo("Tom");
-//        assertThat(tom.getAge()).isEqualTo(21);
-//        assertThat(tom.getClasses().size()).isEqualTo(1);
-//        assertThat(tom.getClasses().toArray()[0]).isEqualTo(klass2);
-//    }
+
+    @Test
+    public void should_teacher_have_name_and_age_and_classes() throws Exception {
+        LinkedList<Klass> linkedList = new LinkedList<Klass>();
+        linkedList.add(klass2);
+        Teacher tom = new Teacher(1, "Tom", 21, linkedList);
+        assertThat(tom.getName()).isEqualTo("Tom");
+        assertThat(tom.getAge()).isEqualTo(21);
+        assertThat(tom.getClasses().size()).isEqualTo(1);
+        assertThat(tom.getClasses().toArray()[0]).isEqualTo(klass2);
+    }
 //
 //    @Test
 //    public void should_teacher_introduce_itself_with_which_classes_it_teaches() throws Exception {
