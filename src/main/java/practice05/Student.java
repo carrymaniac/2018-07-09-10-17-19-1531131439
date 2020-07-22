4e3bce019defe8ff8dbb291611ed4ce4ace688f0
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 15:53:56 +0800

    [henry][feature]pass the test  should_student_introduce_with_class in practice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -19,4 +19,8 @@ public class Student extends Person{
         this.klass = klass;
     }
 
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class "+this.klass+".";
+    }
 }
