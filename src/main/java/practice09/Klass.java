4fc0b80632a145a37e989b04e677c636e64991c8
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 16:47:43 +0800

    [henry][feature]pass the test  should_class_have_a_number in practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,32 @@
 package practice09;
 
+import practice08.Student;
+
 public class Klass {
+    Integer number;
+    practice08.Student leader;
+    public Klass() {
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+    public void assignLeader(practice08.Student student){
+        this.leader = student;
+    }
+    public Student getLeader(){
+        return this.leader;
+    }
 }
--- src/test/java/practice09/Practice09Test.java
+++ src/test/java/practice09/Practice09Test.java
@@ -9,14 +9,14 @@ import java.io.PrintStream;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class Practice09Test {
-//    private Klass klass;
-//    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-//
-//    @Before
-//    public void setup() {
-//        klass = new Klass(2);
-//        System.setOut(new PrintStream(outContent));
-//    }
+    private Klass klass;
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setup() {
+        klass = new Klass(2);
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Test
     public void should_person_have_id_name_and_age() throws Exception {
@@ -38,11 +38,11 @@ public class Practice09Test {
         String introduce = tom.introduce();
         assertThat(introduce).isEqualTo("My name is Tom. I am 21 years old.");
     }
-//
-//    @Test
-//    public void should_class_have_a_number() throws Exception {
-//        assertThat(klass.getNumber()).isEqualTo(2);
-//    }
+
+    @Test
+    public void should_class_have_a_number() throws Exception {
+        assertThat(klass.getNumber()).isEqualTo(2);
+    }
 //
 //    @Test
 //    public void should_class_get_display_name() throws Exception {
