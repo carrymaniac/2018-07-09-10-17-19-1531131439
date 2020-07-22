d61a425a7424bc9097a1c2025ffad3c76e4e7f19
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 18:57:36 +0800

    [henry][feature]pass the test  should_class_not_assign_a_student_as_leader_when_student_is_not_a_member in practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,43 @@
 package practice11;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Klass {
+    Integer number;
+    Student leader;
+    private final Set<Integer> studentMembers;
+    public Klass(Set<Integer> studentMembers) {
+        this.studentMembers = studentMembers;
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+        this.studentMembers = new HashSet<>();
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+    public void appendMember(Student student){
+        studentMembers.add(student.getId());
+    }
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+    public void assignLeader(Student student){
+        if(this.studentMembers.contains(student.id)){
+            this.leader = student;
+        }else {
+            System.out.println("It is not one of us.");
+        }
+    }
+    public Student getLeader(){
+        return this.leader;
+    }
+
 }
--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,31 @@
 package practice11;
 
-public class Student {
+public class Student extends Person{
+    Klass klass;
+
+    public Student(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    public Student(Integer id, String name, Integer age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    @Override
+    public String introduce() {
+        String result = super.introduce()+" I am a Student. ";
+        if(this.equals(this.klass.leader)){
+            return result+"I am Leader of "+this.klass.getDisplayName()+".";
+        }else {
+            return result+"I am at "+this.klass.getDisplayName()+".";
+        }
+    }
 }
