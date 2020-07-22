2892669a55c5493fc8c7ab227929e9f2ec10bbf3
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 16:07:55 +0800

    [henry][feature]pass the test  should_class_have_a_number in practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,20 @@
 package practice07;
 
 public class Klass {
+    Integer number;
+
+    public Klass() {
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 }
