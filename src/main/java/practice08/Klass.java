b71de82ad18b1f7c2125015741327b664f8a5c4f
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-22 16:26:36 +0800

    [henry][feature]pass the test  should_class_get_display_name in practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -17,4 +17,8 @@ public class Klass {
     public void setNumber(Integer number) {
         this.number = number;
     }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
 }
